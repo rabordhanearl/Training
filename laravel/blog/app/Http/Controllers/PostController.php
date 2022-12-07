34b053a21e3b7f0ab117b1d9d6dda5f748d8156c
Dhan Earl Rabor <dhanearl.rabor@nabepero.co.jp>
2022-12-07 18:40:41 +0800

    partial Section 10 output until Part 52

--- laravel/blog/app/Http/Controllers/PostController.php
+++ laravel/blog/app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,7 +14,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        return "its working";
+        $posts = Post::all();
+
+        foreach ($posts as $post) {
+        return $post->title;
+    }
     }
 
     /**
--- laravel/blog/routes/web.php
+++ laravel/blog/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 /*
@@ -12,6 +13,31 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/insert', function () {
+    
+    DB::insert('insert into posts (title, content, author) values (?, ?, ?)', ['Php with Laravel', 'Laravel is a PHP platform', 'Dhan Earl Rabor']);
+});
+
+// Route::get('/read', function(){
+
+//     $results = DB::select('select * from posts where id = ?', [1]);
+
+//     foreach ($results as $post ) {
+//         return $post->content;
+//     }
+// });
+
+// Route::get('/update', function(){
+
+//     return $updated = DB::update('update posts set title = "Fantastic Title" where id = ?', [1]);
+
+// });
+
+// Route::get('/delete', function(){
+
+//     return DB::delete('delete from posts where id = ?', [2]);
+
+// });
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,4 +49,22 @@ Route::get('/post', [PostController::class, 'index']);
 
 Route::get('/contact', [PostController::class, 'showMyContact']);
 
-Route::get('post/{id}/{name}/{age}', [PostController::class, 'showPost']);
+// Route::get('post/{id}/{name}/{age}', [PostController::class, 'showPost']);
+
+Route::get('/read', [PostController::class, 'index']);
+
+// Route::get('/basicupdate', function(){
+//     $post = Post::find(3);
+//     $post->title = "The land of the Beginning";
+//     $post->content = "As I walk down the street, down the street";
+//     $post->author = "Cocomelon";
+//     $post->save();
+// });
+
+Route::get('create', function () {
+    Post::create(['title'=>'Game of Thrones', 'content'=>'Full of Dragons and droggos.', 'author'=>'Jake Winterfall']);
+});
+
+Route::get('/update', function () {
+    Post::where('id', 3)->where('is_admin', 0)->update(['title'=>'Star Wars', 'content'=>'Anakin Skywalker is Gay.', 'author'=>'Darth Vader']);
+});
